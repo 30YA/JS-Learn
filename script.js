6d2554747fb1b6a9cb76eb39f9a39530833f5112
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-10-31 15:14:16 +0330

    array method

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 "use strict";
-function onchange1(){
-    console.log(event.target.value);
-    console.log(`changed ...`);
-}
-function fooking(){
-    console.log(event.ctrlKey);
-    if (event.ctrlKey) {
-        alert(`salam sooton`)
-    }
-    else(
-        alert(`siki girikh`)
-    )
-}
-function over() {
-    console.log(event);
-    event.target.innerHTML="siavash"
-}
-function keydown1() {
-    console.log(event);
-}
+const array = [12,85,50,1];
+array.forEach(function(value) {
+    console.log(value);
+});
+const f = array.filter(function(value) {
+    return value <= 20;
+})
+const m = array.map(function(val) {
+    return val + 2
+})
+const e = array.every(function(val) {
+    return val >= 1;
+})
+const s = array.some(function(val) {
+    return val > 60;
+})
+const i = array.indexOf(85);
+console.log();
