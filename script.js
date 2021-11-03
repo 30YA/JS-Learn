c43c90c410ff70eb5999f2a591bebde18417ad3d
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-11-03 12:54:48 +0330

    setTimeout,setInterval,NestedInterval

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 "use strict";
-const array = [12,85,50,1];
-array.forEach(function(value) {
-    console.log(value);
-});
-const f = array.filter(function(value) {
-    return value <= 20;
-})
-const m = array.map(function(val) {
-    return val + 2
-})
-const e = array.every(function(val) {
-    return val >= 1;
-})
-const s = array.some(function(val) {
-    return val > 60;
-})
-const i = array.indexOf(85);
-console.log();
+function timer(a) {
+    console.log(a);
+    console.log(new Date());
+}
+// -------------------------------------------------
+// ------------------setTimeout---------------------
+// -------------------------------------------------
+// let timerid = setTimeout(timer,2000,"siavash");
+// clearTimeout(timerid);
+// ------------------------
+// let timerid = setTimeout((a) => {
+//     a = "sia";
+//     console.log(a);
+//     console.log(new Date());
+// }, 2000);
+// -------------------------------------------------
+// ------------------setInterval--------------------
+// -------------------------------------------------
+// let interval = setInterval(timer,2000,"amir");
+// clearInterval(interval);
+// ------------------------
+// let interval = setInterval((a) => {
+//     a = "sia";
+//     console.log(a);
+//     console.log(new Date()); 
+// }, 2000);
+// setTimeout(() => {
+//     clearInterval(interval);
+// }, 6000);
+// -------------------------------------------------
+// ---------------Nested setTimeout-----------------
+// -------------------------------------------------
+// setTimeout(function run() {
+//     console.log("siavash");
+//     setTimeout(run,1000);
+// },1000);
