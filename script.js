f49990425fb84d07b7a5516a1a31af9768d72e8b
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-11-07 11:09:01 +0330

    All Operators List

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
 "use strict";
-function timer(a) {
-    console.log(a);
-    console.log(new Date());
-}
-// -------------------------------------------------
-// ------------------setTimeout---------------------
-// -------------------------------------------------
-// let timerid = setTimeout(timer,2000,"siavash");
-// clearTimeout(timerid);
-// ------------------------
-// let timerid = setTimeout((a) => {
-//     a = "sia";
-//     console.log(a);
-//     console.log(new Date());
-// }, 2000);
-// -------------------------------------------------
-// ------------------setInterval--------------------
-// -------------------------------------------------
-// let interval = setInterval(timer,2000,"amir");
-// clearInterval(interval);
-// ------------------------
-// let interval = setInterval((a) => {
-//     a = "sia";
-//     console.log(a);
-//     console.log(new Date()); 
-// }, 2000);
-// setTimeout(() => {
-//     clearInterval(interval);
-// }, 6000);
-// -------------------------------------------------
-// ---------------Nested setTimeout-----------------
-// -------------------------------------------------
-// setTimeout(function run() {
-//     console.log("siavash");
-//     setTimeout(run,1000);
-// },1000);
+// Arithmatic operator : + , - , / , % , * , ** , ++ , --
+// assignment operator : = , += , -= , *= , /= ,.....
+// comparison operator : > , >= , < , <= , == , ===
+// Ternary operator : condition ? true : false
+// Lagical operator : && , || , ! 
+/* Logical With non boolean {
+    falsy => 0 , "" , null , undifind , NaN , false
+    truthy => except the above categoreis 
+}*/
