abda64593af015a3078e0c24e44bdb94ee846b90
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-11-10 15:31:30 +0330

    cloning objects with 3 way

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 "use strict";
-// enumrating object : ------------------------------------
-const obj = {name: "sia", age: 20};
-
-for(let keys in obj){
-  console.log(keys,obj[keys]);
+// cloning objects : ------------------------------------
+// with for loop
+const obj1 = {
+  name: "siavash",
+  age: 24
 }
-
-for(let keys of Object.values(obj)){
-  console.log(keys);
+const obj2 = {};
+for(const key in obj1){
+  obj2[key] = obj1[key];
 }
+obj2.age = 40
+console.log(obj1);
+console.log(obj2);
+console.log("--------- with for in loop");
 
-for(let [keys,value] of Object.entries(obj)){
-    console.log(`${keys} + ${value}`);
-  }
+// with Assign ---------------------
+const obj3 = Object.assign({},obj1);
+obj3.age = 12;
+console.log(obj1);
+console.log(obj3);
+console.log("--------- with object.assigne");
+
+// with spread Operator ------------
+const obj4 = {...obj1};
+obj4.age = 12;
+console.log(obj1);
+console.log(obj4);
+console.log("--------- with spread operator");
