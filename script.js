aa252edc6c1091dd0b673306cac7faf779374f07
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-11-11 15:51:15 +0330

    work with DOM node

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 "use strict";
-// cloning objects : ------------------------------------
-// with for loop
-const obj1 = {
-  name: "siavash",
-  age: 24
-}
-const obj2 = {};
-for(const key in obj1){
-  obj2[key] = obj1[key];
-}
-obj2.age = 40
-console.log(obj1);
-console.log(obj2);
-console.log("--------- with for in loop");
+// work with DOM node : ------------------------------------
 
-// with Assign ---------------------
-const obj3 = Object.assign({},obj1);
-obj3.age = 12;
-console.log(obj1);
-console.log(obj3);
-console.log("--------- with object.assigne");
+let test = document.querySelector('table');
+console.log(test.nodeName);
+console.log(test.tagName);
+console.log(test.innerHTML = "<p>siavash</p>");
+console.log(test.outerHTML = "<aside>sia</aside>");
+console.log(test.outerHTML);
+console.log(test.textContent);
+test.hidden = false;
 
-// with spread Operator ------------
-const obj4 = {...obj1};
-obj4.age = 12;
-console.log(obj1);
-console.log(obj4);
-console.log("--------- with spread operator");
+// const obj_test = {
+//     name: "siavash",
+//     age: 20,
+//     city: "Karaj" 
+// }
+// function showUser(object) {
+//     for(const key in object){
+//         console.log(key,object[key]);
+//     }
+// }
+// showUser(obj);
+//---------------------------------------------------------
