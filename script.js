eb11ff490ef020bf6defdfb74f6d1603592901c9
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-11-13 15:34:54 +0330

    work with creatElement $$ Document Fragment in DOM

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 "use strict";
-// work with Attribute : ------------------------------------
-const test = document.querySelector("table.table-class");
-console.log(test);
-console.log(test.id);
-console.log(test.className);
-console.log(test.getAttribute('id'));
-test.setAttribute("id","table-10")
-console.log(test.hasAttribute("class"));
-test.removeAttribute("class");
+// work with creatElement $$ Document Fragment : ------------------------------------
+const main1 = document.querySelector("main");
+const newElementt = document.createTextNode("Hellow body");
+const newElement = document.createElement("table");
+newElement.id = "table-5";
+newElement.className = "table";
+newElement.innerHTML = "<h1>siavash</h1>";
+// main1.append(newElement);
+// main1.prepend(newElement);
+// main1.after(newElement);
+main1.before(newElement);
+newElement.remove();
+let newElement2 = newElement.cloneNode(true)
+let newElement3 = newElement.cloneNode(false)
+console.log(newElement2,newElement3);
+
+//----------------------Fragment-----------------------------
+const frag = document.createDocumentFragment();
+frag.append(document.createElement("div"));
+frag.append(document.createTextNode("Hellow Mother"));
+document.body.append(frag);
+
+//---------------------------------------------------
+function makeList(where) {
+  if (!(where in main1)) {
+    return alert("siavash");
+  }
+  const newElement = document.createElement("table");
+  newElement.id = "table-5";
+  newElement.className = "table";
+  main1[where](newElement);
+}
+function getPlace() {
+  const userr = prompt("where :");
+  makeList(userr);
+}
