62bf180158bff6e92ea03d1c1f309c71fac093eb
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-11-15 14:06:06 +0330

    Elements-Geography & scroll in DOM

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 "use strict";
-// work with class and style in DOM : ------------------------------------
-const main = document.querySelector("main");
-main.classList.add("main-color","ahmad");
-main.classList.remove("main-color");
-console.log(main.classList.contains("main"));
-main.classList.toggle("fuck");//if have this class ,remove it but if dont have , add it
-console.log(main.classList);
-
-//-------------- getComputedStyle & style-----------
+// work with Elements-Geography & scroll in DOM : ------------------------------------
 const p = document.getElementById("par");
-console.log(p.style);
-p.style.backgroundColor = prompt("enter")
-const p_style = getComputedStyle(p);
-console.log(p_style.padding);
-
-//------------------work with toggle--------------
-const p2 = document.getElementById("par")
-function getPlace() {
-    p2.classList.toggle("sia");
+const circle = document.querySelector(".circle");
+console.log(p.clientWidth);
+console.log(p.scrollHeight);
+function goto() {
+    circle.style.top = p.offsetTop + "px";
+}
+console.log(document.body.offsetWidth);
+/*
+clientWidth
+clientHeight
+offsetWidth
+offsetHeight
+scrollHeight
+scrollTop
+offsetTop
+offsetLeft
+clientTop(border)
+clientLeft(border)
+*/
+function scrolltoo() {
+    // window.scrollTo(0,0);
+    // window.scrollBy(0,10);
+    // example3.scrollBy(0,5);
+    // window.example2.scrollIntoView(true);
+    // window.example2.scrollIntoView(false);
+    // let num = prompt("enter :");
+    // window['example'+num].scrollIntoView(false);
 }
