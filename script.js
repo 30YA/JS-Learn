98fcbf1d33aa5d4182b40bd309005193ab71da12
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-11-16 12:34:12 +0330

    undingClient & ElementFromPoint in DOM

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
 "use strict";
-// work with Elements-Geography & scroll in DOM : ------------------------------------
+// BoundingClient & ElementFromPoint in DOM : ------------------------------------
 const p = document.getElementById("par");
-const circle = document.querySelector(".circle");
-console.log(p.clientWidth);
-console.log(p.scrollHeight);
-function goto() {
-    circle.style.top = p.offsetTop + "px";
-}
-console.log(document.body.offsetWidth);
-/*
-clientWidth
-clientHeight
-offsetWidth
-offsetHeight
-scrollHeight
-scrollTop
-offsetTop
-offsetLeft
-clientTop(border)
-clientLeft(border)
-*/
-function scrolltoo() {
-    // window.scrollTo(0,0);
-    // window.scrollBy(0,10);
-    // example3.scrollBy(0,5);
-    // window.example2.scrollIntoView(true);
-    // window.example2.scrollIntoView(false);
-    // let num = prompt("enter :");
-    // window['example'+num].scrollIntoView(false);
+const b = p.getBoundingClientRect();
+console.log(b);
+document.onmousemove = function () {
+    const hoverElem = document.elementFromPoint(event.pageX,event.pageY);
+    if (hoverElem.tagName !== "BODY") {
+        console.log(hoverElem);
+    }
 }
