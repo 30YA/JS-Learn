87f4ffc81ac4882701e234f3e0f9b7a1a880247c
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-11-18 13:19:36 +0330

    rest operator in function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 "use strict";
-// BoundingClient & ElementFromPoint in DOM : ------------------------------------
-const p = document.getElementById("par");
-const b = p.getBoundingClientRect();
-console.log(b);
-document.onmousemove = function () {
-    const hoverElem = document.elementFromPoint(event.pageX,event.pageY);
-    if (hoverElem.tagName !== "BODY") {
-        console.log(hoverElem);
-    }
+// rest operator : ------------------------------------
+
+function test() {
+    const arr = [...arguments];
+    let r = arr.reduce((pre,val) => {return pre += val},0)
+    console.log(r);
 }
+test(5,4,7,3);
+
+function test(...argu) {
+    let r = argu.reduce((pre,val) => {return pre + val},0);
+    return r;
+}
+console.log(test(5,4,7,3));
