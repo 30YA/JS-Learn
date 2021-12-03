115fcfadedcd0fcd72898a7b6866a565c5fd4f60
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-12-03 13:04:35 +0330

    Try and catch && Error handeling (part 2)

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 "use strict";
-// filter of products project : ------------------------------------
-const box = document.querySelector(".center .box");
-const spans = document.querySelectorAll(".center .box2 .heading span");
-const main = document.querySelector("body .main");
-document.addEventListener('scroll',function (event) {
-    let scrollOffset = window.pageYOffset;
-    if (scrollOffset <= 500) {
-        let p = 1 + (scrollOffset / 500 );
-        box.style.transform = `scale(${p})`;
-        box.style.opacity = 1;
-        spans[0].style.transform = `translate(0,0)`
-        spans[1].style.transform = `translate(0,0)`
+// try and catch && Error handeling : ----------------------------------
+const obj = {};
+try {
+    console.log(obj.test.sia);
+} catch (error) {
+    console.dir(error);
+    alert("siavash");
+}
+//------------------------------------------
+const array = [];
+function randomnum() {
+    if (array.length == 10) {
+        throw new Error("out of range");
     }
-    if (scrollOffset > 450 && scrollOffset <= 550) {
-        let op = 1 - (scrollOffset / 500);
-        box.style.opacity = op;
+    let rand = Math.floor(Math.random() * 10);
+    if (array.includes(rand)) {
+        console.log("get fuuuuuuuuuuuuuuuuck");
+        return randomnum();
     }
-    else if (scrollOffset >= 500) {
-        let ychanger = scrollOffset - 550;
-        spans[0].style.transform = `translate(0,-${ychanger}px)`
-        spans[1].style.transform = `translate(0,${ychanger}px)`
-    }
-    else if (scrollOffset >= 550) {
-        main.style.transform = `translate(0,-100px)`
-    }
-})
+    array.push(rand);
+    return rand;
+}
+for (let x = 0; x < 155; x++) {
+    console.log(randomnum());
+}
+console.log(array);
