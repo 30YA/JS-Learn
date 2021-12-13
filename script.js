9357a1aabd9fd9077ea899a9443ea10d1aff6ee2
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-12-13 20:34:37 +0330

    work with Local Storage & Session Storage

--- script.js
+++ script.js
@@ -1,45 +1,11 @@
 "use strict";
-// XMLHttp Request & fetch & Api & JSON : ----------------------------------
-// with GET : --------------------------------------------------------------
-const xhr = new XMLHttpRequest();
-xhr.open("GET","https://jsonplaceholder.typicode.com/users");
-xhr.onload = () => {
-    console.log(JSON.parse(xhr.response));
-    console.log(xhr.status);
+// work with local Storage & Session Storage : ----------------------------------
+localStorage.setItem("name","siavash aghazadeh");
+const obj = {
+  name: "siavash",
+  age: 22
 }
-xhr.send();
-
-// with POST : -------------------------------------------------------------
-const xhr2 = new XMLHttpRequest();
-xhr2.open("POST","https://jsonplaceholder.typicode.com/users");
-xhr2.onload = () => {
-    console.log(JSON.parse(xhr2.response));
-    console.log(xhr2.status);
-}
-const siavash = {
-    name: "siavash",
-    age: 23
-}
-xhr2.setRequestHeader("Content-type", "Application/JSON")
-xhr2.send(JSON.stringify(siavash));
-
-// with fetch : -------------------------------------------------------------
-// fetch GET :
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-  
-// fetch POST :
-fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+localStorage.setItem("obj", JSON.stringify(obj));
+console.log(JSON.parse(localStorage.getItem("obj")));
+localStorage.removeItem("name");
+localStorage.clear();
