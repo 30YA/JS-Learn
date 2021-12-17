be4d2d72e35fc131778c4a3b3e41b4cbfe851cef
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-12-17 13:03:29 +0330

    filter of products project (v2) with local storage

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
 "use strict";
-// work with local Storage & Session Storage : ----------------------------------
-localStorage.setItem("name","siavash aghazadeh");
-const obj = {
-  name: "siavash",
-  age: 22
+// : ----------------------------------
+let products = [];
+const form = document.querySelector(".myForm");
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let inputValue = event.target.input1.value;
+  event.target.input1.value = '';
+  addtoBody(inputValue);
+  saveLocal(inputValue);
+})
+function addtoBody(value) {
+  const pTag = document.createElement('p');
+  pTag.textContent = value;
+  pTag.classList = "par";
+  document.querySelector('.products').append(pTag);
 }
-localStorage.setItem("obj", JSON.stringify(obj));
-console.log(JSON.parse(localStorage.getItem("obj")));
-localStorage.removeItem("name");
-localStorage.clear();
+console.log();
+//------------------------------------------------------------------------------------
+function saveLocal(value) {
+  const savedP = localStorage.getItem('pTags')?
+  JSON.parse(localStorage.getItem('pTags')) : 
+  [];
+  savedP.push(value);
+  localStorage.setItem('pTags',JSON.stringify(savedP));
+}
+window.addEventListener('load',loadLocal);
+function loadLocal() {
+  const savedP = localStorage.getItem('pTags')?
+  JSON.parse(localStorage.getItem('pTags')) : 
+  [];
+  savedP.forEach( item => {
+    const pTag = document.createElement('p');
+    pTag.textContent = item
+    pTag.classList.add("par");
+    document.querySelector('.products').append(pTag);
+  })
+}
+//------------------------------------------------------------------------------------
+const searchBox = document.getElementById('search')
+let searchText = '';
+function filterItem(products,searchText) {
+  const filteredItem = products.filter( item => {
+    return item.textContent.toLowerCase().includes(searchText.toLowerCase());
+  })
+  document.querySelector('.siavash').innerHTML = '';
+  filteredItem.forEach(e => {
+    const pTag = document.createElement('p');
+    pTag.textContent = e.textContent
+    pTag.classList.add("par");
+    document.querySelector('.siavash').append(pTag);
+  });
+}
+searchBox.addEventListener('input', (item) => {
+  products = [...document.querySelector('.products').children];
+  searchText = item.target.value;
+  filterItem(products,searchText);
+})
+//--------------------------------------------------------------------------------------
