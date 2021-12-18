154d2708f1ae0481a30f1238482030493b393ab0
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-12-18 16:51:47 +0330

    filter of products project (v3)  with available products

--- script.js
+++ script.js
@@ -1,59 +1,75 @@
 "use strict";
 // : ----------------------------------
-let products = [];
-const form = document.querySelector(".myForm");
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  let inputValue = event.target.input1.value;
-  event.target.input1.value = '';
-  addtoBody(inputValue);
-  saveLocal(inputValue);
-})
-function addtoBody(value) {
+const products = [
+  {title: "booke1", ava: false},
+  {title: "booke2", ava: true},
+  {title: "booke3", ava: false},
+  {title: "booke4", ava: false},
+]
+products.forEach((items) => {
   const pTag = document.createElement('p');
-  pTag.textContent = value;
-  pTag.classList = "par";
+  pTag.classList.add('par');
+  pTag.textContent = items.title;
   document.querySelector('.products').append(pTag);
-}
-console.log();
-//------------------------------------------------------------------------------------
-function saveLocal(value) {
-  const savedP = localStorage.getItem('pTags')?
-  JSON.parse(localStorage.getItem('pTags')) : 
-  [];
-  savedP.push(value);
-  localStorage.setItem('pTags',JSON.stringify(savedP));
-}
-window.addEventListener('load',loadLocal);
-function loadLocal() {
-  const savedP = localStorage.getItem('pTags')?
-  JSON.parse(localStorage.getItem('pTags')) : 
-  [];
-  savedP.forEach( item => {
+})
+const form = document.querySelector('.myForm');
+form.addEventListener('submit',(e) => {
+  e.preventDefault();
+  let inputValue = e.target.input1.value;
+  e.target.input1.value = '';
+  addProducts(inputValue);
+})
+//add Products To DOM :
+function addProducts(value) {
+  document.querySelector('.products').textContent = '';
+  const items = {title:`${value}`, ava: true};
+  //Error handeling
+  if (items.title !== '') {
+    products.push(items);
+  }else{
+    throw new Error('you Did not enter value');
+  }
+  //add new products to DOM : 
+  products.forEach((items) => {
     const pTag = document.createElement('p');
-    pTag.textContent = item
-    pTag.classList.add("par");
+    pTag.classList.add('par');
+    pTag.textContent = items.title;
     document.querySelector('.products').append(pTag);
   })
 }
-//------------------------------------------------------------------------------------
-const searchBox = document.getElementById('search')
-let searchText = '';
-function filterItem(products,searchText) {
-  const filteredItem = products.filter( item => {
-    return item.textContent.toLowerCase().includes(searchText.toLowerCase());
-  })
-  document.querySelector('.siavash').innerHTML = '';
-  filteredItem.forEach(e => {
-    const pTag = document.createElement('p');
-    pTag.textContent = e.textContent
-    pTag.classList.add("par");
-    document.querySelector('.siavash').append(pTag);
-  });
-}
-searchBox.addEventListener('input', (item) => {
-  products = [...document.querySelector('.products').children];
-  searchText = item.target.value;
-  filterItem(products,searchText);
+// -------------------------------------------------------
+//search
+//--------------------------------------------------------
+let filterVal = {
+  inputVal: '',
+  checkbox: true
+};
+const search = document.getElementById('search');
+search.addEventListener('input',(e) => {
+  filterVal.inputVal = e.target.value;
+  renderProducts(products,filterVal);
 })
-//--------------------------------------------------------------------------------------
+function renderProducts(products,filterVal,checkbox) {
+  let filteredItem = products.filter( item => {
+    return item.title.toLowerCase().includes(filterVal.inputVal.toLowerCase());
+  })
+  if (checkbox == true) {
+    filteredItem = filteredItem.filter( item => {
+      return item.ava == true;
+    })
+  }
+  document.querySelector('.products').innerHTML = '';
+  filteredItem.forEach((items) => {
+    const pTag = document.createElement('p');
+    pTag.classList.add('par');
+    pTag.textContent = items.title;
+    document.querySelector('.products').append(pTag);
+  })
+}
+// -------------------------------------------------------
+//checkbox
+//--------------------------------------------------------
+const checkbox = document.querySelector('.check');
+checkbox.addEventListener('change', (e) => {
+  renderProducts(products,filterVal,e.target.checked);
+})
