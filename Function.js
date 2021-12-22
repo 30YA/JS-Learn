4b0d72925f277d07254aaba445c602537fd30d5c
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-12-22 14:54:53 +0330

    Remove Items with their id in product's array

--- Function.js
+++ Function.js
@@ -4,10 +4,10 @@ function GetSavedProducts() {
     return localStorage.getItem('products') ?
     JSON.parse(localStorage.getItem('products')) :
     [
-      {title: "booke1", ava: false},
-      {title: "booke2", ava: true},
-      {title: "booke3", ava: false},
-      {title: "booke4", ava: false},
+      {id: uuidv4(), title: "booke1", ava: false},
+      {id: uuidv4(), title: "booke2", ava: true},
+      {id: uuidv4(), title: "booke3", ava: false},
+      {id: uuidv4(), title: "booke4", ava: false},
     ];
 }
 function saveItems(products) {
@@ -15,7 +15,11 @@ function saveItems(products) {
 }
 function addProducts(value) {
     document.querySelector('.products').textContent = '';
-    const items = {title:`${value}`, ava: true};
+    const items = {
+      id: `${uuidv4()}`,
+      title:`${value}`,
+      ava: true
+    };
     //Error handeling
     if (items.title !== '') {
       products.push(items);
@@ -24,12 +28,7 @@ function addProducts(value) {
       throw new Error('you Did not enter value');
     }
     //add new products to DOM : 
-    products.forEach((items) => {
-      const pTag = document.createElement('p');
-      pTag.classList.add('par');
-      pTag.textContent = items.title;
-      document.querySelector('.products').append(pTag);
-    })
+    creatProductDOM(products)
   }
 function renderProducts(products,filterVal,checkbox) {
     let filteredItem = products.filter( item => {
@@ -40,14 +39,62 @@ function renderProducts(products,filterVal,checkbox) {
         return item.ava == true;
       })
     }
-    creatProductDOM(filteredItem)
-  }
-function creatProductDOM(filteredItem) {
     document.querySelector('.products').innerHTML = '';
     filteredItem.forEach((items) => {
+      const divTag = document.createElement('div');
+      const checkbox = document.createElement('input');
       const pTag = document.createElement('p');
-      pTag.classList.add('par');
+      const RMbutton = document.createElement('input');
+
+      checkbox.setAttribute('type','checkbox');
+      divTag.appendChild(checkbox);
+
       pTag.textContent = items.title;
-      document.querySelector('.products').append(pTag);
-    }) 
+      pTag.classList.add('par');
+      divTag.appendChild(pTag);
+
+      RMbutton.setAttribute('type','button');
+      RMbutton.setAttribute('value','Reamove');
+      RMbutton.classList.add('button');
+      RMbutton.addEventListener('click', item => {
+        removeProducts(items.id);
+      })
+      divTag.appendChild(RMbutton);
+
+      document.querySelector('.products').append(divTag);
+    })
+  }
+function creatProductDOM(products) {
+  products.forEach((items) => {
+    const divTag = document.createElement('div');
+    const checkbox = document.createElement('input');
+    const pTag = document.createElement('p');
+    const RMbutton = document.createElement('input');
+
+    checkbox.setAttribute('type','checkbox');
+    divTag.appendChild(checkbox);
+
+    pTag.textContent = items.title;
+    pTag.classList.add('par');
+    divTag.appendChild(pTag);
+
+    RMbutton.setAttribute('type','button');
+    RMbutton.setAttribute('value','Reamove');
+    RMbutton.classList.add('button');
+    RMbutton.addEventListener('click', () => {
+      removeProducts(items.id);
+    })
+    divTag.appendChild(RMbutton);
+
+    document.querySelector('.products').append(divTag);
+  })
+}
+// Reamove products --------------------------
+function removeProducts(id) {
+  const productIndex = products.findIndex( item => {
+    return item.id === id;
+  })
+  products.splice(productIndex,1);
+  saveItems(products);
+  renderProducts(products,filterVal,checkbox);
 }
--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 "use strict";
-// : ----------------------------------
+// Remove Items with their id in product's array
+// make uuid: ----------------------------------
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+//--------------------------------------
 let products = GetSavedProducts();
+creatProductDOM(products);
 
-products.forEach((items) => {
-  const pTag = document.createElement('p');
-  pTag.classList.add('par');
-  pTag.textContent = items.title;
-  document.querySelector('.products').append(pTag);
-})
 const form = document.querySelector('.myForm');
 form.addEventListener('submit',(e) => {
   e.preventDefault();
@@ -34,4 +36,8 @@ search.addEventListener('input',(e) => {
   filterVal.inputVal = e.target.value;
   renderProducts(products,filterVal);
 })
-renderProducts(products,filterVal,checkbox);
+// renderProducts(products,filterVal,checkbox);
+
+// -------------------------------------------------------
+// remove items
+//--------------------------------------------------------
