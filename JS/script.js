84b81167f361445aaf4bd1729ac55e51fde33a48
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-12-27 15:25:56 +0330

    work with Moment & jalali-moment-js

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,36 @@
 "use strict";
-// Remove Items with their id in product's array
+// work with Moment & jalali-moment-js;
+const now = moment();
+now.locale('fa');
+const month = now.format('MMMM');
+console.log(month);
+console.log(now.format('YYYY-MMMM-DD'));
+console.log(moment().toString());
 //--------------------------------------
-let products = GetSavedProducts();
-creatProductDOM(products);
+// let products = GetSavedProducts();
+// creatProductDOM(products);
 
-const form = document.querySelector('.myForm');
-form.addEventListener('submit',(e) => {
-  e.preventDefault();
-  let inputValue = e.target.input1.value;
-  e.target.input1.value = '';
-  //add Products To DOM :
-  addProducts(inputValue);
-})
+// const form = document.querySelector('.myForm');
+// form.addEventListener('submit',(e) => {
+//   e.preventDefault();
+//   let inputValue = e.target.input1.value;
+//   e.target.input1.value = '';
+//   //add Products To DOM :
+//   addProducts(inputValue);
+// })
 // -------------------------------------------------------
 //checkbox
 //--------------------------------------------------------
-const checkbox = document.querySelector('.check');
-checkbox.addEventListener('change', (e) => {
-  renderProducts(products,filterVal,e.target.checked);
-})
+// const checkbox = document.querySelector('.check');
+// checkbox.addEventListener('change', (e) => {
+//   renderProducts(products,filterVal,e.target.checked);
+// })
 // -------------------------------------------------------
 //search
 //--------------------------------------------------------
-const search = document.getElementById('search');
-search.addEventListener('input',(e) => {
-  filterVal.inputVal = e.target.value;
-  renderProducts(products,filterVal);
-})
+// const search = document.getElementById('search');
+// search.addEventListener('input',(e) => {
+//   filterVal.inputVal = e.target.value;
+//   renderProducts(products,filterVal);
+// })
 // -------------------------------------------------------
