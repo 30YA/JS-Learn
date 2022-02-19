e5b2477d4cffb036cb091da753ca97a6ba626de6
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2022-02-19 16:31:02 +0330

    webpack.config.js (Entry , Output)

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/JS/script.js',
-    output: {
-        path: path.resolve(__dirname, './public/JS'),
-        filename: 'bundle.js'
-    }
-}
+  entry: {
+      script: './src/JS/script.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'public/JS'),
+    filename: '[name].bundle.js',
+  },
+};
