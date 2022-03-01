d05df30beb6c3ff44aeee225d62e3e85b4512df1
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2022-03-01 14:47:45 +0330

    webpack: stat & ProgressPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 const config = {
   entry: {
@@ -13,6 +14,10 @@ const config = {
     path: path.resolve(__dirname, "./public"),
     // publicPath: "public/",
     // assetModuleFilename: "fonts/[name].[ext]",
+  },
+  //stats: 'errors-only',
+  stats: {
+    assets: false,
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -28,6 +33,7 @@ const config = {
       chunks: ["script", "admin"],
     }),
     new CleanWebpackPlugin(),
+    new webpack.ProgressPlugin(),
   ],
   module: {
     rules: [
